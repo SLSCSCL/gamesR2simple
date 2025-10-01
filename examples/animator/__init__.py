60cefe5b421da9251402aa82b64b4ae864bcd688
SLSCSCL <168966013+SLSCSCL@users.noreply.github.com>
2025-10-01 06:41:23 +0400

    Updated __init__.py

--- examples/animator/__init__.py
+++ examples/animator/__init__.py
@@ -1,13 +1,43 @@
-from gamesR2simple import BaseGame
+from gamesR2simple import BaseGame, keys
 
-from .file_loader import FileLoader
+from project_handler import *
 
 class Game(BaseGame):
     def setup(self):
+        self.drawing = {
+            "kind": "c",
+            "radius": 5
+        }
+        
         self.set_screen_size(1000, 700)
+        
+        self.events.add_quit_event(keys["Escape"])
+        self.events.add_mouse_event("move", self._plot)
+        self.events.add_mouse_event("down", self._plot)
+        
+        self.draw.fill_style = (255, 255, 255)
+
+    def _plot(self, pos):
+        if pos[0] > 200 and pos[1] < 500:
+            match self.drawing["kind"]:
+                case "r":
+                    self.draw.fill_rect(
+                        pos[0],
+                        pos[1],
+                        self.drawing["width"],
+                        self.drawing["height"]
+                    )
+                case "c":
+                    self.draw.fill_circle(
+                        pos[0],
+                        pos[1],
+                        self.drawing["radius"]
+                    )
 
     def update(self):
-        pass
+        self.draw.stroke_style = (100, 100, 100)
+        self.draw.line_width = 5
+        self.draw.stroke_rect(200, 0, 800, 500)
 
 if __name__ == "__main__":
     Game()
