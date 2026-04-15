0d1b75421d3d9c6706fac81e60e4d57b1fcce958
Rayhaan H. Aubrey <168966013+SLSCSCL@users.noreply.github.com>
2026-04-15 16:37:23 +0400

    Delete src/events directory
    Signed-off-by: Rayhaan H. Aubrey <168966013+SLSCSCL@users.noreply.github.com>

--- src/events/_error_msgs.py
+++
@@ -1,5 +0,0 @@
-event_errs = {
-    "overridden_event": "{} events must not already exist as a {} event",
-    "mouse_e_callback_arg_undef": "Mouse{} events callbacks require 1 " +
-        "positional argument for the mouse pos"
-}
