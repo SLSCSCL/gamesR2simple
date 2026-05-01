486040ee387dd7ee534324bbdb65b69dcda6cb9d
SLSCSCL <168966013+SLSCSCL@users.noreply.github.com>
2026-05-01 12:01:08 +0400

    Refactor key_mouse_combination.py for clarity
    Signed-off-by: SLSCSCL <168966013+SLSCSCL@users.noreply.github.com>

--- src/examples/key_mouse_combination.py
+++ src/examples/key_mouse_combination.py
@@ -1,10 +1,10 @@
-from gamesR2simple import BaseGameWindow, run, keys, MouseKeyCombination
+from gamesR2simple import BaseGameWindow, run, keys, MouseKeyCombination#, quit
 
 class Game(BaseGameWindow):
     def setup(self):
         self.create_window("Mouse-key event combination example", 800, 400)
         self.events.add_mouse_key_combination(
-            MouseKeyCombination(keys["x"], lambda x, y: print("x-click"))
+            MouseKeyCombination(keys["x"], lambda x, y: print("x-click"))#quit) #doesn't work - throws an error, will fix soon
         )
         
     def update(self):
